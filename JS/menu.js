f92847b57e35b70ecdba3d841bcce85d7d07c442
florine89 <ferreiraflorine@gmail.com>
2023-04-04 16:33:12 +0200

    menu done

--- JS/menu.js
+++ JS/menu.js
@@ -1,7 +1,32 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "350px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+// function openNav() {
+//   document.querySelector(".sidenav").style.width = "350px";
+//   console.log(openNav)
+// }
+
+//   function openNav() {
+//     const element = document.getElementById("mySidenav").style.width = "350px";
+//     const etat = element.style.display;
+ 
+//     if ( etat == none )
+//     element.style.display = 'block';
+//     else
+//     element.style.display = 'none';
+//    }
+   
+//     function closeNav() {
+//      document.getElementById("mySidenav").style.width = "0";
+//    }
+
+
+// Avec Jquery:
+function show() {
+    const menu = $(".sidenav");
+
+    menu.css({"display" : "block", 
+         "Transition": '1',});
+
+}
+
+function closeNav() {
+    $(".sidenav").css('display', 'none',)
+}
